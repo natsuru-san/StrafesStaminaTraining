9286394c34cbf7d960b64c2db52814b27922080c
natsuru <natsuru-san@mail.com>
2023-04-05 13:26:58 +0300

    Small changes

--- app/src/main/java/karelia/natsuru/strafesstaminatraining/MainActivity.java
+++ app/src/main/java/karelia/natsuru/strafesstaminatraining/MainActivity.java
@@ -2,8 +2,20 @@ package karelia.natsuru.strafesstaminatraining;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView timer;
+    private ImageButton start;
+    private ImageButton score;
+    private ImageButton options;
+    private Button wBtn;
+    private Button aBtn;
+    private Button sBtn;
+    private Button dBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
-
+        timer = findViewById(R.id.Timer);
+        start = findViewById(R.id.StartButton);
+        score = findViewById(R.id.ScoreButton);
+        options = findViewById(R.id.OptionsButton);
+        wBtn = findViewById(R.id.WButton);
+        aBtn = findViewById(R.id.AButton);
+        sBtn = findViewById(R.id.SButton);
+        dBtn = findViewById(R.id.DButton);
     }
 }
