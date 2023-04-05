5afddc105ea7312d75429cf3aad1067f27620d6d
natsuru <natsuru-san@mail.com>
2023-04-05 13:18:41 +0300

    Initial commit

---
+++ app/src/main/java/karelia/natsuru/strafesstaminatraining/MainActivity.java
@@ -0,0 +1,18 @@
+package karelia.natsuru.strafesstaminatraining;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+        setContentView(R.layout.activity_main);
+    }
+
+    private void init() {
+
+    }
+}
